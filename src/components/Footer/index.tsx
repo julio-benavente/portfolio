16095500f29e7a60403ca8615d4b0e8a697875f8
Julio Benavente <juliobenavente@Julios-MacBook-Pro-2.local>
2023-07-31 19:39:44 +0200

    feat: footer created

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import C from "@/components";
+
+import { GitHub, LinkedIn } from "@mui/icons-material";
+
+const index = () => {
+  return (
+    <footer className="bg-primaryBg">
+      <div className="mx-6 py-16">
+        <div className="flex flex-col items-center gap-4">
+          <h3 className="text-3xl text-center">Julio B.</h3>
+          <p className="text-lg text-center">
+            This website has been created based on the template Meelo by{" "}
+            <a href="https://webflow.com/templates/designers/elemis">elemis</a>
+          </p>
+          <div className="flex flex-row gap-4">
+            <C.ButtonIcon
+              icon={GitHub}
+              as="a"
+              href="https://github.com/julio-benavente"
+            />
+            <C.ButtonIcon
+              icon={LinkedIn}
+              as="a"
+              href="linkedin.com/in/julio-benavente-504ba9119/"
+            />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default index;
