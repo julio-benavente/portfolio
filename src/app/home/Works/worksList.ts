a8c7c7b0d6ae01d2ea1ae35b4dce7ae1e59aa2db
Julio Benavente <julio.benavente.02@gmail.com>
2023-08-03 14:30:00 +0200

    feat: new project added

--- src/app/home/Works/worksList.ts
+++ src/app/home/Works/worksList.ts
@@ -1,4 +1,5 @@
 import ProjectOneImage from "@/../public/assets/images/project-one-image.png";
+import ProjectTwoImage from "@/../public/assets/images/project-two-image.png";
 
 const worksList = [
   {
@@ -11,16 +12,16 @@ const worksList = [
     githubLink: "https://github.com/julio-benavente/portfolio",
     websiteLink: "https://juliobenavente.com/",
   },
-  // {
-  //   name: "Sandbox Banking Application Website 2",
-  //   image: ProjectImage,
-  //   imageAlt: "Lorem ipsum dolor sit.",
-  //   description:
-  //     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem iure rem eaque possimus tenetur. Optio vitae sapiente voluptate asperiores quaerat.",
-  //   caseStudyLink: "https://juliobenavente.com/",
-  //   githubLink: "https://github.com/",
-  //   websiteLink: "https://juliobenavente.com/",
-  // },
+  {
+    name: "Sheriful",
+    image: ProjectTwoImage,
+    imageAlt: "Lorem ipsum dolor sit.",
+    description:
+      "It's an application I'm working on at the moment. It's a platform that allows people to share their digital content and ask a help from the people who wants acces. This help consists on completing a 'social taks' such as subscribing, following, sharing or liking. For the frontend I'm using NextJS and Tailwind and MaterialUI. For the backend I'm planning on using AppWrite (BaaS).",
+    // caseStudyLink: "https://juliobenavente.com/",
+    // githubLink: "https://github.com/",
+    // websiteLink: "https://juliobenavente.com/",
+  },
   // {
   //   name: "Sandbox Banking Application Website 3",
   //   image: ProjectImage,
