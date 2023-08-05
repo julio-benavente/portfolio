3432e9c3f6a960667797c145f3a0e0a9fed0c453
Julio Benavente <julio.benavente.02@gmail.com>
2023-08-05 18:22:33 +0200

    fix: real testimonials added

--- src/app/home/Experience/testimonialsList.ts
+++ src/app/home/Experience/testimonialsList.ts
@@ -1,36 +1,36 @@
 const testimonialsList = [
   {
     id: 0,
-    person: "Elvis Sanchez",
-    position: "Tech Lead",
-    company: "AFP Habitat",
-    testimonial:
-      '"Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus facilisis in, egestas eget quam. Praesent commodo cursus magna, scelerisque nisl consectetur."',
-  },
-  {
-    id: 1,
     person: "Luis Saco",
     position: "Scrum Master",
     company: "AFP Habitat",
     testimonial:
-      '"Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus facilisis in, egestas eget quam. Praesent commodo cursus magna, scelerisque nisl consectetur."',
+      '"I had the privilege of working with Julio Benavente on various. From the beginning, I was able to highlight his good attitude, his extensive knowledge, and his willingness to learn. He was always willing to provide support to others and share his knowledge. His contribution to the team was invaluable, as he was always collaborative and willing to work as a team."',
   },
   {
-    id: 2,
-    person: "Nataly Porras",
-    position: "Fullstack Developer",
-    company: "AFP Habitat",
+    id: 1,
+    person: "Judy Lou Bloomfield",
+    position: "Founder and CEO",
+    company: "Ink An' Paper",
     testimonial:
-      '"Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus facilisis in, egestas eget quam. Praesent commodo cursus magna, scelerisque nisl consectetur."',
+      '"Julio is super upbeat and great to work with. He has a great attitude. For every idea I had, his response was positive. He really helped my website come to life."',
   },
-  {
-    id: 3,
-    person: "Eduardo Mejias",
-    position: "UI/UX Designer",
-    company: "Solera Systems",
-    testimonial:
-      '"Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus facilisis in, egestas eget quam. Praesent commodo cursus magna, scelerisque nisl consectetur scelerisque nisl consectetur."',
-  },
+  // {
+  //   id: 2,
+  //   person: "Nataly Porras",
+  //   position: "Fullstack Developer",
+  //   company: "AFP Habitat",
+  //   testimonial:
+  //     '"Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus facilisis in, egestas eget quam. Praesent commodo cursus magna, scelerisque nisl consectetur."',
+  // },
+  // {
+  //   id: 3,
+  //   person: "Eduardo Mejias",
+  //   position: "UI/UX Designer",
+  //   company: "Solera Systems",
+  //   testimonial:
+  //     '"Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus facilisis in, egestas eget quam. Praesent commodo cursus magna, scelerisque nisl consectetur scelerisque nisl consectetur."',
+  // },
 ];
 
 export default testimonialsList;
